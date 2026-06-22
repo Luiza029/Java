6b2d7e7b0f371f4a19e423b298e22e96b555335b
Luiza Costa <cwluiza029@gmail.com>
2026-06-22 12:56:32 -0300

    Atribuição de Variáveis

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Ola Mundo");
 		
+		int idade;
+		double salario, altura;
+		char genero;
+		String nome;
+		
+		idade = 20;
+		salario = 2.000;
+		altura = 1.65;
+		genero = 'F';
+		nome = "Luiza";
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Salario: " + salario);
+		System.out.println("Altura: "+ altura);
+		System.out.println("Genero: " + genero);
+		System.out.println("Nome: " + nome);
 	}
 
 }
