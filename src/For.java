3b420a1118a479ffdcffce28ba3da301ba86b9ac
Luiza Costa <cwluiza029@gmail.com>
2026-06-23 09:57:04 -0300

    Estrutura for

---
+++ src/For.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class For {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n, i, x, soma = 0;
+		
+		System.out.println("Digite quantos numeros serao digitados: ");
+		n = sc.nextInt();
+		
+		for(i=0; i<n; i++) {
+			System.out.println("Digite um numero: ");
+			x = sc.nextInt();
+			
+			soma += x;
+		}
+		
+		System.out.println("Soma: " + soma);
+	}
+
+}
