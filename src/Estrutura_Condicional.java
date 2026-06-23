ea59af433c61427015f15c578f35da19ebd30843
Luiza Costa <cwluiza029@gmail.com>
2026-06-23 09:45:44 -0300

    Condicional

---
+++ src/Estrutura_Condicional.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Estrutura_Condicional {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		int hora;
+		System.out.println("Digite uma hora do dia: ");
+		hora = sc.nextInt();
+		
+		if(hora < 12) {
+			System.out.println("Bom Dia");
+		}
+		
+		else if(hora < 18){
+			System.out.println("Boa Tarde");
+		}
+		
+		else {
+			System.out.println("Boa noite");
+		}
+		
+		sc.close();
+	}
+
+}
