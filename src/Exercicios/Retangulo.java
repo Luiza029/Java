ec5c3fe84a9d7975d8e7799266329c36766ed4ee
Luiza Costa <cwluiza029@gmail.com>
2026-06-24 11:00:07 -0300

    Exercicio 01

--- src/Exercicios/Retangulo.java
+++ src/Exercicios/Retangulo.java
@@ -1,10 +1,27 @@
 package Exercicios;
-
+import java.util.Scanner;
 public class Retangulo {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-
+		double base, altura, area, peri, diag;
+		
+		System.out.println("Digite a Base do retangulo: ");
+		base = sc.nextDouble();
+		
+		System.out.println("Digite a altura do retangulo: ");
+		altura = sc.nextDouble();
+		
+		area = base * altura;
+		peri = 2 * (base + altura);
+		diag = (base * base) + (altura* altura);
+		
+		System.out.println("Area: " + area);
+		System.out.println("Perimetro: " + peri);
+		System.out.println("Diagonal: " + String.format("%.4f",Math.sqrt(diag)));
+		
+		sc.close();
 	}
 
 }
