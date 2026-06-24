dc6a81902398cb92d26f3601a7a8f03583c30c4c
Luiza Costa <cwluiza029@gmail.com>
2026-06-24 10:01:50 -0300

    Vetores

---
+++ src/Vetores.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Vetores {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n, i;
+		
+		System.out.println("Digite quantos numeros serao digitados: ");
+		n = sc.nextInt();
+		
+		double[] vet = new double[n];
+		
+		for(i=0; i<n; i++) {
+			System.out.println("Digite um numero: ");
+			vet[i] = sc.nextDouble();
+		}
+		
+		System.out.println("Numeros digitados: ");
+		for(i=0; i<n; i++) {
+			System.out.println(String.format("%.1f", vet[i]));
+		}
+	}
+
+}
